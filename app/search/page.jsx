57d37aa59dcd2aaa2328de6d3aa4d57db2493f5d
SultanaJahan25 <coursesellerbd25@gamil.com>
2026-04-30 21:08:26 +0600

    fix search app router prerender issue

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,15 +1,7 @@
-"use client";
-
 export const dynamic = "force-dynamic";
 
-import { Suspense } from "react";
 import SearchContent from "./SearchContent";
 
-
 export default function SearchPage() {
-    return (
-        <Suspense fallback={<p>Loading...</p>}>
-            <SearchContent />
-        </Suspense>
-    );
+    return <SearchContent />;
 }
